4a5da4166662cf0e2f36b6a065e6c2755dd09623
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2019-12-27 23:20:22 +0000

    Added changes to project logging interface

--- projectInterface_mainui/logInterface_mainui.cpp
+++ projectInterface_mainui/logInterface_mainui.cpp
@@ -1,5 +1,4 @@
 #include <cstdarg>
-#include <memory>
 #include "logInterface_mainui.h"
 #include "enginecallback_menu.h"
 #include "utlstring.h"
@@ -7,68 +6,70 @@
 #define PREFIX_WARNING "^3Warning:^7 "
 #define PREFIX_ERROR "^1Error:^7 "
 
+CFixedSizeMessageBuffer<ILogInterface::MAX_MESSAGE_LENGTH> LogInterface_MainUI::m_MsgBuffer;
+
 void LogInterface_MainUI::Log(ILogInterface::Level level, const CUtlString& message)
 {
-	switch ( level )
-	{
-		case ILogInterface::Level::Debug:
-		{
-			Con_DPrintf(const_cast<char*>(message.String()));
-			break;
-		}
-
-		case ILogInterface::Level::Warning:
-		{
-			Con_Printf(PREFIX_WARNING "%s", message.String());
-			break;
-		}
-
-		case ILogInterface::Level::Error:
-		{
-			Con_Printf(PREFIX_ERROR "%s", message.String());
-			break;
-		}
-
-		default:
-		{
-			Con_Printf(const_cast<char*>(message.String()));
-			break;
-		}
-	}
+	LogMessage(level, message.String());
 }
 
 void LogInterface_MainUI::LogF(ILogInterface::Level level, const char* format, ...)
 {
-	std::unique_ptr<char> message(new char[MAX_MESSAGE_LENGTH]);
+	if ( !format )
+	{
+		return;
+	}
+
 	va_list args;
 
 	va_start(args, format);
-	V_vsnprintf(message.get(), MAX_MESSAGE_LENGTH, format, args);
+	m_MsgBuffer.VFormat(format, args);
 	va_end(args);
+
+	LogMessage(level, m_MsgBuffer.Buffer());
+}
+
+void LogInterface_MainUI::LogV(Level level, const char* format, va_list args)
+{
+	if ( !format )
+	{
+		return;
+	}
+
+	m_MsgBuffer.VFormat(format, args);
+	LogMessage(level, m_MsgBuffer.Buffer());
+}
+
+void LogInterface_MainUI::LogMessage(Level level, const char* message)
+{
+	if ( !message )
+	{
+		return;
+	}
 
 	switch ( level )
 	{
 		case ILogInterface::Level::Debug:
 		{
-			Con_DPrintf(message.get());
+			Con_DPrintf(message);
 			break;
 		}
 
 		case ILogInterface::Level::Warning:
 		{
-			Con_Printf(PREFIX_WARNING "%s", message.get());
+			Con_Printf(PREFIX_WARNING "%s", message);
 			break;
 		}
 
 		case ILogInterface::Level::Error:
 		{
-			Con_Printf(PREFIX_ERROR "%s", message.get());
+			Con_Printf(PREFIX_ERROR "%s", message);
 			break;
 		}
 
 		default:
 		{
-			Con_Printf(message.get());
+			Con_Printf(message);
 			break;
 		}
 	}
